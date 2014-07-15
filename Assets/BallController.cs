56c9d42fa7407c0060b485767da3d82a627c40cd
Hiroyuki Yabe <yabe@cyber.t.u-tokyo.ac.jp>
2014-07-16 05:10:01 +0900

    initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce((transform.forward*2 + transform.right) * speed, ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/HitPlaySound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPlaySound : MonoBehaviour {
+
+	public AudioClip sound;
+		void OnCollisionEnter(Collision collision) {
+			AudioSource.PlayClipAtPoint(sound, transform.position);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
